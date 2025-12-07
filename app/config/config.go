37923c86f208a804417b0e8b4580399210fda60e
lawnmower-74 <tonapricot@gmail.com>
2025-12-07 23:14:02 +0900

    セマフォの設定値が最小でも1となるよう修正（0となった場合のデッドロック回避）

--- app/config/config.go
+++ app/config/config.go
@@ -26,7 +26,12 @@ func loadConfig() {
 	AppConfig.ImageDir = "/images/upload"
 
 	// 並列・並行含め同時に処理できるタスクの最大数
-	AppConfig.MaxWorkers = runtime.NumCPU()
+	// runtime.NumCPU()が0を返す可能性があるため、最小値1を保証
+	numCPU := runtime.NumCPU()
+	if numCPU < 1 {
+		numCPU = 1
+	}
+	AppConfig.MaxWorkers = numCPU
 
 	log.Println("------- 設定値 -------")
 	log.Printf("AppConfig: %#v", AppConfig)
